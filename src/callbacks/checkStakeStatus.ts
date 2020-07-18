80f02a668df5b0d5d47ea41856ac8a9ab4741c9e
kot-shrodingera <kot.shrodingera@gmail.com>
2020-07-18 13:46:23 +0600

    Исправления функции определения результата ставки
    Вывод в лог текста сообщения об изменениях в купоне, если таковой имеется
    Исправлен CSS класс сообщения об ошибке ставки
    Обновлён формат информа в ТГ при появлении сообщения о необходимости проверки результата ставки самостоятельно в My Bets
    Вывод в лог текста ошибки ставки, если это не сообщение о проверке в My Bets
    Ставка считается не принятой, при любой ошибке, кроме проверки в My Bets

--- src/callbacks/checkStakeStatus.ts
+++ src/callbacks/checkStakeStatus.ts
@@ -3,6 +3,7 @@ import {
   // betslipDoneSelector,
   betslipPlaceBetButtonTextSelector,
   standardBetslipSelector,
+  betslipAcceptChangesButtonSelector,
 } from '../selectors';
 import { checkRestriction, accountBlocked } from '../accountChecks';
 import updateBalance from '../stakeInfo/updateBalance';
@@ -10,6 +11,23 @@ import updateBalance from '../stakeInfo/updateBalance';
 const checkStakeStatus = (): boolean => {
   if (checkRestriction()) {
     accountBlocked();
+    return false;
+  }
+
+  const acceptButton = document.querySelector(
+    betslipAcceptChangesButtonSelector
+  ) as HTMLElement;
+  if (acceptButton) {
+    const footerMessage = document.querySelector('.bss-Footer_MessageBody');
+    if (footerMessage) {
+      worker.Helper.WriteLine(
+        `Ставка не принята: В купоне были изменения (${footerMessage.textContent.trim()})`
+      );
+    } else {
+      worker.Helper.WriteLine(
+        `Ставка не принята: В купоне были изменения (текст изменений не найден)`
+      );
+    }
     return false;
   }
   const betslipHeaderText = document.querySelector(betslipHeaderTextSelector);
@@ -44,29 +62,39 @@ const checkStakeStatus = (): boolean => {
     return true;
   }
   const betslipPlaceBetErrorMessageElement = document.querySelector(
-    '.bs-PlaceBetErrorMessage'
+    '.bs-PlaceBetErrorMessage_Contents'
   );
-  if (
-    betslipPlaceBetErrorMessageElement &&
-    betslipPlaceBetErrorMessageElement.textContent.trim() ===
+  if (betslipPlaceBetErrorMessageElement) {
+    const errorText = betslipPlaceBetErrorMessageElement.textContent.trim();
+    if (
+      errorText ===
       'Please check My Bets for confirmation that your bet has been successfully placed.'
-  ) {
-    const message =
-      `В Bet365 появилось окно о необходимости проверки принятия ставки в "My Bets"\n` +
-      `Бот засчитал ставку как проставленную\n` +
-      `Событие: ${worker.TeamOne} - ${worker.TeamTwo}\n` +
-      `Ставка: ${worker.BetName}\n` +
-      `Сумма: ${worker.StakeInfo.Summ}\n` +
-      `Пожалуйста, проверьте самостоятельно. Если всё плохо - пишите в ТП`;
-    worker.Helper.WriteLine(message);
-    worker.Helper.SendInformedMessage(message);
+    ) {
+      const message =
+        `В Bet365 появилось окно о необходимости проверки принятия ставки в "My Bets"\n` +
+        `Бот засчитал ставку как проставленную\n` +
+        `Событие: ${worker.TeamOne} - ${worker.TeamTwo}\n` +
+        `Ставка: ${worker.BetName}\n` +
+        `Сумма: ${worker.StakeInfo.Summ}\n` +
+        `Коэффициент: ${worker.StakeInfo.Coef}\n` +
+        `Пожалуйста, проверьте самостоятельно. Если всё плохо - пишите в ТП`;
+      worker.Helper.WriteLine(message);
+      worker.Helper.SendInformedMessage(message);
+      const betslipPlaceBetErrorMessageButton = document.querySelector(
+        '.bs-PlaceBetErrorMessage_Button'
+      ) as HTMLElement;
+      if (betslipPlaceBetErrorMessageButton) {
+        betslipPlaceBetErrorMessageButton.click();
+      }
+      return true;
+    }
+    worker.Helper.WriteLine(`Ошибка ставки: ${errorText}`);
     const betslipPlaceBetErrorMessageButton = document.querySelector(
       '.bs-PlaceBetErrorMessage_Button'
     ) as HTMLElement;
     if (betslipPlaceBetErrorMessageButton) {
       betslipPlaceBetErrorMessageButton.click();
     }
-    return true;
   }
   worker.Helper.WriteLine('Ставка не принята');
   return false;

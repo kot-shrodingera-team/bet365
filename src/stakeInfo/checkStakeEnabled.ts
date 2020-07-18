7c3b8357e9c13474cf3ea794b653be5ef7344444
kot-shrodingera <kot.shrodingera@gmail.com>
2020-07-18 13:36:36 +0600

    Исправлено определение доступности ставки
    Ставка недоступна, если нет открытых купонов
    Недоступость также определяется по элементу .bsm-BetslipStandardModule, в частности, если купон не виден (не раскрыт), то ставка считается недоступной
    Обновлён CSS класс недоступной ставки в купоне

--- src/stakeInfo/checkStakeEnabled.ts
+++ src/stakeInfo/checkStakeEnabled.ts
@@ -1,19 +1,30 @@
 import { checkRestriction, accountBlocked } from '../accountChecks';
-import { betslipBetSelector, betslipBetSuspendedClass } from '../selectors';
 import checkBet from '../checkBet';
+import getStakeCount from './getStakeCount';
 
 const checkStakeEnabled = (): boolean => {
   if (checkRestriction()) {
     accountBlocked();
     return false;
   }
-  const bet = document.querySelector(betslipBetSelector);
+  if (getStakeCount() !== 1) {
+    return false;
+  }
+  const betslipModule = document.querySelector('.bsm-BetslipStandardModule');
+  if (
+    betslipModule &&
+    ![...betslipModule.classList].includes('bsm-BetslipStandardModule_Expanded')
+  ) {
+    worker.Helper.WriteLine('Купон не отображается');
+    return false;
+  }
+  const bet = document.querySelector('.bss-NormalBetItem');
   if (!bet) {
     console.log('Нет ставки');
     return false;
   }
   // let betslipBetSuspended = bet.querySelector(betslipBetSuspendedSelector);
-  if ([...bet.classList].includes(betslipBetSuspendedClass)) {
+  if ([...bet.classList].includes('bss-NormalBetItem_Suspended')) {
     worker.Helper.WriteLine('Ставка Suspended');
     return false;
   }

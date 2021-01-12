ced88ab00c80272b41af315acca25d12ba7bbbd3
kot-shrodingera <kot.shrodingera@gmail.com>
2021-01-12 18:38:27 +0600

    fix(changeToStandardBetslip): другой метод перключения купона

--- src/show_stake/changeToStandardBetslip.ts
+++ src/show_stake/changeToStandardBetslip.ts
@@ -1,26 +1,52 @@
-import { getElement, fireEvent, log } from '@kot-shrodingera-team/germes-utils';
+import { getElement, log, awaiter } from '@kot-shrodingera-team/germes-utils';
+import getStakeCount from '../stake_info/getStakeCount';
 
 const changeToStandardBetslip = async (): Promise<boolean> => {
-  const qbsStakeInput = document.querySelector(
-    '.qbs-StakeBox_StakeInput'
+  const anyBet = document.querySelector(
+    '.ovm-ParticipantOddsOnly:not(.ovm-ParticipantOddsOnly_Highlighted)'
   ) as HTMLElement;
-  if (!qbsStakeInput) {
-    log('Не найдена кнопка ввода суммы в мобильном купоне', 'crimson');
+  if (!anyBet) {
+    log('Не найдена любая ставка', 'crimson', true);
     return false;
   }
-  qbsStakeInput.click();
-  const qbsAddToBetslipButton = await getElement('.qbs-AddToBetslipButton');
-  if (!qbsAddToBetslipButton) {
-    log('Не найдена кнопка переключения купона на стандартный', 'crimson');
+  anyBet.click();
+  const minibizedBetslip = (await getElement(
+    '.bss-BetslipStandardModule_Minimised .bss-DefaultContent'
+  )) as HTMLElement;
+  if (!minibizedBetslip) {
+    log('Не найден свернутый купон', 'crimson', true);
     return false;
   }
-  fireEvent(qbsAddToBetslipButton, 'touchstart', TouchEvent);
-  fireEvent(qbsAddToBetslipButton, 'touchend', TouchEvent);
-  const normalBetItem = await getElement(
-    '.bss-NormalBetItem_FixtureDescription'
+  log('Разворачиваем свёрнутый купон', 'orange', true);
+  minibizedBetslip.click();
+  const expandedBetslip = await getElement(
+    '.bss-BetslipStandardModule_Expanded'
   );
-  if (!normalBetItem) {
-    log('Стандартный купон не появился', 'crimson');
+  if (!expandedBetslip) {
+    log('Не найден развёрнутый купон', 'crimson', true);
+    return false;
+  }
+  await awaiter(() => {
+    return document.querySelectorAll('.bss-NormalBetItem_Remove').length === 2;
+  });
+  const deleteButtons = document.querySelectorAll(
+    '.bss-NormalBetItem_Remove'
+  ) as NodeListOf<HTMLElement>;
+  if (deleteButtons.length !== 2) {
+    log(
+      `В купоне не 2 кнопки удаления ставки (${deleteButtons.length})`,
+      'crimson',
+      true
+    );
+    return false;
+  }
+  log('Удаляем вторую ставку', 'orange', true);
+  deleteButtons[1].click();
+  const betDeleted = await awaiter(() => {
+    return getStakeCount() === 1;
+  });
+  if (!betDeleted) {
+    log('Не дождались появления 1 ставки в купоне', 'crimson', true);
     return false;
   }
   return true;

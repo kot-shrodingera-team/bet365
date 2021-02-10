f72af05c9181f58c6aeb8d71403c2c3595b224b3
kot-shrodingera <kot.shrodingera@gmail.com>
2021-02-11 00:57:20 +0600

    fix(updateMaximumStake): обработка нового сообщения о максе

--- src/stake_info/getMaximumStake.ts
+++ src/stake_info/getMaximumStake.ts
@@ -24,6 +24,34 @@ export const updateMaximumStake = (): boolean => {
       maximumStake = Number(maxErrorMatch[1]);
       return true;
     }
+    const newMaxError = /^Your stake exceeds the maximum allowed$/.test(
+      betErrorMessage
+    );
+    if (newMaxError) {
+      const newMaxStakeValueElement = document.querySelector(
+        '.bss-NormalBetItem_MessageBody'
+      );
+      if (!newMaxStakeValueElement) {
+        log('Есть ошибка о максе (новая), но нет самого макса', 'crimson');
+        maximumStake = 0;
+        return true;
+      }
+      const newMaxStakeValueText = newMaxStakeValueElement.textContent.trim();
+      const newMaxErrorMatch = newMaxStakeValueText.match(
+        /^Max Stake .*(\d+\.\d+)\.$/i
+      );
+      if (!newMaxErrorMatch) {
+        log(
+          `Есть ошибка о максе (новая), но не понятен формат самого макса: "${newMaxStakeValueText}"`,
+          'crimson'
+        );
+        maximumStake = 0;
+        return true;
+      }
+      log(`Обновлена максимальная ставка: "${newMaxErrorMatch[1]}"`, 'orange');
+      maximumStake = Number(newMaxErrorMatch[1]);
+      return true;
+    }
   }
   return false;
 };
